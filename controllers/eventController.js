3e1f0f14b9f294e999895618ec3859f2f8dda499
Kunal Sapate <113632918+kunal20000@users.noreply.github.com>
2023-12-03 19:33:47 +0530

    Update eventController.js

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -9,6 +9,20 @@ const jwt = require('jsonwebtoken');
 const createEvent = async (req, res) => {
   try {
     //Write a logic for createEvent
+     const { name, date } = req.body;
+
+    // Assuming that you have a user ID from the decoded JWT token
+    const creatorId = req.user._id;
+
+    const newEvent = new Event({
+      name,
+      date,
+      creator: creatorId,
+    });
+
+    const savedEvent = await newEvent.save();
+
+    res.status(201).json(savedEvent);
   } catch (err) {
     console.error(err);
     res.status(500).send('Server Error');
@@ -19,6 +33,20 @@ const createEvent = async (req, res) => {
 const inviteUser = async (req, res) => {
   try {
     //Write a logic to invite a user
+    const { eventId, inviteeId } = req.body;
+
+    // Assuming you have the creator's ID from the decoded JWT token
+    const creatorId = req.user._id;
+
+    const newInvitee = new Invitee({
+      eventId,
+      creator: creatorId,
+      invitee: inviteeId,
+    });
+
+    const savedInvitee = await newInvitee.save();
+
+    res.status(201).json(savedInvitee);
   } catch (err) {
     console.error(err);
     res.status(500).send('Server Error');
@@ -30,6 +58,13 @@ const updateEvent = async (req, res) => {
   const { name, date } = req.body;
   try {
     //Write a logic to update name and date of a event
+    const updatedEvent = await Event.findByIdAndUpdate(
+      id,
+      { $set: { name, date } },
+      { new: true }
+    );
+
+    res.status(200).json(updatedEvent);
   } catch (err) {
     console.error(err);
     res.status(500).send('Server Error');
